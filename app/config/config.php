40e29fc1c283d31083c6ccc785e53820f089b42b
centino90 <anthonyjayansit2326282@gmail.com>
2021-11-27 23:37:07 +0800

    test

--- app/config/config.php
+++ app/config/config.php
@@ -1,25 +1,24 @@
 <?php
   // DB Params
   define('DB_HOST', 'localhost');
-  define('DB_USER', 'root');
-  define('DB_PASS', '');
-  define('DB_NAME', 'DuesPaymentSystem');
+  define('DB_USER', 'u704967255_centino90');
+  define('DB_PASS', 'Tropawzpoge123');
+  define('DB_NAME', 'u704967255_PaymentSystem');
 
   // fb credentials
-	define( 'FB_APP_ID', '597967324656000' );
-	define( 'FB_APP_SECRET', '96b5f8442fa44b01bc8ee03d17da0271' );
-	define( 'FB_REDIRECT_URI', 'http://localhost/DuesPaymentSystem/users/login' );
+	define( 'FB_APP_ID', '4649891738473679' );
+	define( 'FB_APP_SECRET', '3f981db9771fa424b0870a9eb4e4a4f4' );
+	define( 'FB_REDIRECT_URI', 'https://www.pda-dcc.com/users/login' );
 
   // GOOGLE credentials
-  define( 'GOOGLE_CLIENT_ID', '339949192946-oeiqofcjp8j81t759usbnm5eamp20uvn.apps.googleusercontent.com' );
-	define( 'GOOGLE_CLIENT_SECRET', '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' );
-	define( 'GOOGLE_REDIRECT_URI', 'http://localhost/DuesPaymentSystem/users/login' );
+  define( 'GOOGLE_CLIENT_ID', '716149094685-4no609952083305njj3n077nbd3kf61r.apps.googleusercontent.com' );
+	define( 'GOOGLE_CLIENT_SECRET', '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' );
+	define( 'GOOGLE_REDIRECT_URI', 'https://www.pda-dcc.com/users/login' );
 
-  // App Root
+// APP ROOT
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/DuesPaymentSystem');
-  // Site Name
-  define('SITENAME', 'DuesPaymentSystem');
+  define('URLROOT', 'https://www.pda-dcc.com');
   // App Version
-  define('APPVERSION', '1.0.0');
+  define('APPVERSION', '1.0.0'); 
+  define('SITENAME', 'PDO-DCC');
