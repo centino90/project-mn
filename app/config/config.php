90b1a2fd95c79806cea1faef3168ac022458571c
centino90 <anthonyjayansit2326282@gmail.com>
2021-12-23 23:03:35 +0800

    test

--- app/config/config.php
+++ app/config/config.php
@@ -3,11 +3,10 @@ define( 'FB_GRAPH_VERSION', 'v6.0' ); // facebook graph version
 define( 'FB_GRAPH_DOMAIN', 'https://graph.facebook.com/' ); // base domain for api
 define( 'FB_APP_STATE', 'eciphp' ); // verify state
 
-// APP ROOT
+// App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'https://www.pda-dcc.com');
-
+define('URLROOT', 'http://localhost/DuesPaymentSystem');
 // Site Name
 define('SITENAME', 'PDA-DCC');
 // App Version
