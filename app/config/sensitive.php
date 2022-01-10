ad2016c29117b4583f0551ecf265c02be2f32a4b
centino90 <anthonyjayansit2326282@gmail.com>
2022-01-10 20:37:02 +0800

    test

--- app/config/sensitive.php
+++ app/config/sensitive.php
@@ -1,30 +1,30 @@
 <?php
 // DB Params
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'DuesPaymentSystem');
+define('DB_USER', 'u704967255_centino90');
+define('DB_PASS', 'Tropawzpoge123');
+define('DB_NAME', 'u704967255_PaymentSystem');
 
 // fb credentials
-define('FB_APP_ID', '597967324656000');
-define('FB_APP_SECRET', '96b5f8442fa44b01bc8ee03d17da0271');
-define('FB_REDIRECT_URI', 'http://localhost/DuesPaymentSystem/users/redirectPage');
+define('FB_APP_ID', '4649891738473679');
+define('FB_APP_SECRET', '3f981db9771fa424b0870a9eb4e4a4f4');
+define('FB_REDIRECT_URI', 'https://www.pda-dcc.com/users/redirectPage');
 
 // GOOGLE credentials
-define('GOOGLE_CLIENT_ID', '339949192946-oeiqofcjp8j81t759usbnm5eamp20uvn.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', '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');
-define('GOOGLE_REDIRECT_URI', 'http://localhost/DuesPaymentSystem/users/redirectPage');
+define('GOOGLE_CLIENT_ID', '716149094685-4no609952083305njj3n077nbd3kf61r.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET', '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');
+define('GOOGLE_REDIRECT_URI', 'https://www.pda-dcc.com/users/redirectPage');
 
-// RECAPTCHA credentials
+// Google RECAPTCHA credentials
 define('RECAPTCHA_SITEKEY', '6LcpA30dAAAAAOVfY5fIQgV5BDZsAtBnpo8lkdW1');
 define('RECAPTCHA_SECRET', '6LcpA30dAAAAAGQnG5gzHeDcSiOMEfEaz8_93-a1');
 
 // MAIL settings
 define('MAIL_MAILER', 'smtp');
-define('MAIL_HOST', 'smtp.mailtrap.io');
+define('MAIL_HOST', 'smtp.hostinger.com');
 define('MAIL_PORT', '587');
-define('MAIL_USERNAME', '5751f0c27c5469');
-define('MAIL_PASSWORD', '9a4457a2f0275c');
+define('MAIL_USERNAME', 'admin@pda-dcc.com');
+define('MAIL_PASSWORD', 'MH7PHcJj3:d2];kYym]dC9)u}');
 define('MAIL_ENCRYPTION', null);
-define('MAIL_FROM_ADDRESS', 'c2c439914d-5fcf45@inbox.mailtrap.io');
+define('MAIL_FROM_ADDRESS', 'admin@pda-dcc.com');
 define('MAIL_FROM_NAME', 'PDA-DCC');
